6bb1e8baa571e7e3052477ba27c2c9335a5b649a
Daniel Müller <daniel.mueller@uni-muenster.de>
2024-06-04 12:45:01 +0200

    change params

--- lib/Controller/LaunchController.php
+++ lib/Controller/LaunchController.php
@@ -81,8 +81,8 @@ class LaunchController extends Controller
     private function getOAuth2QueryParams(array &$queryParams): void
     {
         if (array_key_exists("state", $_GET) && strtolower($_GET["state"]) == "code" && array_key_exists("code", $_GET)) {
-            $queryParams["oauth2:auth"] = "request";
-            $queryParams["oauth2:auth-code"] = $_GET["code"];
+            $queryParams["auth:action"] = "request";
+            $queryParams["auth:code"] = $_GET["code"];
         }
     }
 }
