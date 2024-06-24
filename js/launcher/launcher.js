9821cf93b5f5aff49882875dde505baeeafd993f
Daniel Müller <daniel.mueller@uni-muenster.de>
2024-06-24 12:16:48 +0200

    react to messages from the app

--- js/launcher/launcher.js
+++ js/launcher/launcher.js
@@ -6,4 +6,27 @@ $(document).ready(() => {
         $("#app-frame").show();
         $('#app-frame').css('background-color', '#fff');
     });
+
+    // The integrated app occasionally communicates with the host through messages
+    window.addEventListener(
+        "message",
+        (event) => {
+            // Make sure that the message came from the embedded app
+            if (event.source.parent !== window) {
+                return;
+            }
+
+            // Commands come in the form { action: '...', data: <any> }
+            const data = event.data;
+            if (data.hasOwnProperty("action")) {
+                switch (event.data["action"]) {
+                    case "redirect":
+                        if (data.hasOwnProperty("data")) {
+                            window.location.replace(data["data"]);
+                        }
+                }
+            }
+        },
+        false,
+    );
 });
